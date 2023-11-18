317db808a98456d19e38d332dbd43efa094d6341
Llorenç Buil Fustagueras <ygbuil@Llorencs-MacBook-Air.local>
2023-11-18 14:02:45 +0100

    fixed start button location

--- gui/button_actions.py
+++ gui/button_actions.py
@@ -1,7 +1,46 @@
 import json
-
+import time
+import threading
 from tkinter import filedialog
 from constants import *
+
+
+def start_backup(x, y):
+    print("Start")
+    time.sleep(3)
+    print("End")
+
+
+def btn_function(app, button, progress_bar):
+    x = 100
+    y = 100
+
+    thread = threading.Thread(target=start_backup, args=(x, y))
+    thread.start()
+
+    # block button
+    button.configure(state="disabled")
+
+    # show progressbar
+    progress_bar.pack(pady=40)
+
+    # start updating progressbar
+    update_progressbar(
+        app=app, button=button, progress_bar=progress_bar, thread=thread
+    )  # send thread as parameter - so it doesn't need `global`
+
+
+def update_progressbar(app, button, progress_bar, thread):
+    if thread.is_alive():
+        # update progressbar
+        progress_bar.step()
+        # check again after 25ms
+        app.after(25, update_progressbar, app, button, progress_bar, thread)
+    else:
+        # hide progressbar
+        progress_bar.pack_forget()
+        # unblock button
+        button.configure(state="normal")
 
 
 def update_label_cache(cache):
--- gui/gui.py
+++ gui/gui.py
@@ -1,5 +1,3 @@
-import time
-import threading
 import json
 import customtkinter as ctk
 import button_actions
@@ -13,10 +11,6 @@ with open(LABEL_CACHE, "r") as json_file:
 
 app = ctk.CTk()
 app.title("Smart Backup")
-
-
-oytter_frame = ctk.CTkFrame(app)
-oytter_frame.pack(padx=20, pady=20, fill="both", expand=True)
 
 title = ctk.CTkLabel(app, text="Smart Backup")
 title.pack(pady=10, padx=10)
@@ -37,7 +31,7 @@ origin_frame.add_label(
     else cache["origin"]
 )
 
-#
+
 # backup frame
 backup_frame = Frame.create_frame(
     app,
@@ -57,48 +51,23 @@ backup_frame.add_label(
 )
 
 
-def start_backup(x, y):
-    print("Start")
-    time.sleep(3)
-    print("End")
-
-
-def btn_function():
-    x = 100
-    y = 100
-
-    boolean_progress_bar = threading.Thread(target=start_backup, args=(x, y))
-    boolean_progress_bar.start()
-
-    # block button
-    button.configure(state="disabled")
-
-    # show progressbar
-    progress_bar.pack(pady=40)
-
-    # start updating progressbar
-    update_progressbar(
-        boolean_progress_bar
-    )  # send thread as parameter - so it doesn't need `global`
-
-
-def update_progressbar(thread):
-    if thread.is_alive():
-        # update progressbar
-        progress_bar.step()
-        # check again after 25ms
-        app.after(25, update_progressbar, thread)
-    else:
-        # hide progressbar
-        progress_bar.pack_forget()
-        # unblock button
-        button.configure(state="normal")
-
-
-progress_bar = ctk.CTkProgressBar(app, mode="indeterminate", indeterminate_speed=3)
-
-button = ctk.CTkButton(app, text="Start", command=btn_function)
-button.pack(pady=10)
+trigger_frame = Frame.create_frame(
+    app,
+    frame_type="start_button",
+    x_frame=X_FRAME_TRIGGER,
+    y_frame=Y_FRAME_TRIGGER,
+)
+trigger_frame.add_button(
+    button_text="Start",
+    command=lambda: button_actions.btn_function(
+        app=app,
+        button=trigger_frame.button,
+        progress_bar=ctk.CTkProgressBar(
+            app, mode="indeterminate", indeterminate_speed=3
+        ),
+    ),
+    width=200,
+)
 
 
 set_window_geometry(app)
