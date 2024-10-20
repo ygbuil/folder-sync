f1537a296904cda84f422869cd703d1b402d222f
Llorenç Buil Fustagueras <ygbuil@Llorencs-MacBook-Air.local>
2024-10-20 15:10:35 +0200

    fix tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,5 +1,5 @@
-from backend.main import main
+from backend.entry_points._pipeline import _pipeline
 
 
-def test_process(origin_folder, destination_folder, trigger_object):
-    assert main(origin_folder, destination_folder, trigger_object)[0] == 0
+def test_process(origin_folder, destination_folder, trigger_object) -> None:
+    assert _pipeline(origin_folder, destination_folder, trigger_object)[0] == 0
